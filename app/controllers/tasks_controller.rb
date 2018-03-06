23cd077ae84677f41e75210313acf271bd75e6ea
shoma otuka <ootuka@tohkon.co.jp>
2018-03-06 05:51:51 +0000

    taskac

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
-	 before_action :set_task, only: [:show, :edit, :update, :destroy]
+	 #before_action :set_task, only: [:show, :edit, :update, :destroy]
+	 before_action :correct_user, only: [:edit, :update, :show, :destroy]
 	
 	def index
-		@tasks = Task.all.page(params[:page]).per(10)
+		if logged_in?
+      @user = current_user
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page]).per(10)
+    end
 	end
 	
   def show
@@ -14,7 +18,8 @@ class TasksController < ApplicationController
   end
 
   def create
-  	@task = Task.new(task_params)
+  # 	@task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスク が正常に投稿されました'
@@ -50,14 +55,20 @@ class TasksController < ApplicationController
 	
 	private
 	
-	def set_task
-		 @task = Task.find(params[:id])
-	end
+# 	def set_task
+# 		 @task = Task.find(params[:id])
+# 	end
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
 	
+	def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 	
 end
